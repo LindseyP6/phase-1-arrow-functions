8f3ef55e574531082effd51b5b2f255e9ef5c6dc
Lindsey <lindsey.pregent@gmail.com>
2022-01-11 12:06:11 -0500

    commit tree 85fa06bc9847c3b8b03f1dcdb54219918f89a9c8

---
+++ index.js
@@ -0,0 +1,8 @@
+//const twoAdder = x => x + 2;
+//const add = (parameter1, parameter2) => parameter1 + parameter2
+
+const divide = (a, b) => 2000 / 100;
+
+const square = x => x * x;
+
+const add = (a, b) => 3 + 4;
